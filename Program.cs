eca1e7a3c7456e341f958b5eff09e541844efb59
RyanMrts <ryanmartinssilva2006@gmail.com>
2023-03-24 20:51:40 -0300

    exercicio pronto

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+string frasedigitada, frasecebolinha;
+
+
+
+Console.WriteLine("Digite uma frase: ");
+frasedigitada = Console.ReadLine()!;
+
+frasecebolinha = frasedigitada
+.Replace("r", "l")
+.Replace("R", "L");
+Console.WriteLine(frasecebolinha);
+
+
+
+
